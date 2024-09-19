b1a9339fbd34283105c93fbebdbd5203ab0624df
dave-budah <davebudah@gmail.com>
2024-09-19 11:05:44 +0200

    Page redesigning completed.

--- src/app/layouts/public/public.routes.ts
+++ src/app/layouts/public/public.routes.ts
@@ -18,30 +18,37 @@ export const routes: Route[] = [
       {
         path: 'cooperatives',
         loadComponent: () => import('../../pages/public/cooperatives/cooperatives.component').then(m => m.CooperativesComponent),
+        title: 'Cooperatives'
       },
       {
         path: 'individuals',
         loadComponent: () => import('../../pages/public/individuals/individuals.component').then(m => m.IndividualsComponent),
+        title: 'Individuals'
       },
       {
         path: 'card-issuers',
         loadComponent: () => import('../../pages/public/card-issuers/card-issuers.component').then(m => m.CardIssuersComponent),
+        title: 'Card Issuers'
       },
       {
         path: 'merchants',
         loadComponent: () => import('../../pages/public/merchants/merchants.component').then(m => m.MerchantsComponent),
+        title: 'Merchants'
       },
       {
         path: 'governments',
         loadComponent: () => import('../../pages/public/governments/governments.component').then(m => m.GovernmentsComponent),
+        title: 'Governments'
       },
       {
         path: 'contact',
         loadComponent: () => import('../../pages/public/contact/contact.component').then(m => m.ContactComponent),
+        title: 'Contact'
       },
       {
         path: 'impact',
         loadComponent: () => import('../../pages/public/impact/impact.component').then(m => m.ImpactComponent),
+        title: 'Impact'
       }
     ]
   }
