2afb63f15d8b3f54c6a29f077593e290c676e87a
dave-budah <davebudah@gmail.com>
2024-09-23 12:39:29 +0200

    Redundant code removed

--- src/app/pages/public/merchants/merchants.component.ts
+++ src/app/pages/public/merchants/merchants.component.ts
@@ -5,63 +5,7 @@ import {Component, ElementRef, HostListener, inject, Renderer2} from '@angular/c
   standalone: true,
   imports: [],
   templateUrl: './merchants.component.html',
-  styleUrl: './merchants.component.css'
 })
 export class MerchantsComponent {
-  renderer = inject(Renderer2)
-  el = inject(ElementRef)
-  private animationElements!: NodeListOf<Element>
 
-
-  ngOnInit(): void {
-    const scrollers = document.querySelectorAll('.scroller')
-
-    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-      addAnimation()
-    }
-    function addAnimation() {
-      scrollers.forEach((scroller) => {
-        scroller.setAttribute('data-animated', String(true))
-
-        const scrollerInner = scroller.querySelector('.scroller__inner')
-        const scrollerContent = Array.from(scrollerInner!.children)
-
-        scrollerContent.forEach((item) => {
-          const duplicatedItem = item.cloneNode(true)
-          // @ts-ignore
-          duplicatedItem.setAttribute('aria-hidden', String(true))
-          scrollerInner!.appendChild(duplicatedItem)
-        })
-      })
-    }
-  }
-
-  ngAfterViewInit(): void {
-    this.animationElements = this.el.nativeElement.querySelectorAll('.animation-element');
-    this.checkIfInView();
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  @HostListener('window:resize', ['$event'])
-  onWindowChange(): void {
-    this.checkIfInView();
-  }
-
-  checkIfInView(): void {
-    const windowHeight = window.innerHeight;
-    const windowTopPosition = window.scrollY;
-    const windowBottomPosition = windowTopPosition + windowHeight;
-
-    this.animationElements.forEach((element: any) => {
-      const elementHeight = element.offsetHeight;
-      const elementTopPosition = element.getBoundingClientRect().top + window.scrollY;
-      const elementBottomPosition = elementTopPosition + elementHeight;
-
-      if ((elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition)) {
-        this.renderer.addClass(element, 'in-view');
-      } else {
-        this.renderer.removeClass(element, 'in-view');
-      }
-    });
-  }
 }
