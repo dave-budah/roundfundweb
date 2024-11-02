27af914190dca254406f84571e6ebaac6a378e08
dave-budah <davebudah@gmail.com>
2024-11-02 19:39:46 +0200

    Color and logo update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     loadChildren: () => import('./layouts/public/public.routes').then(m => m.routes)
