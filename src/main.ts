3c22ad8d89df51cf87f62aa7117ccbc1c2804b3c
dave-budah <davebudah@gmail.com>
2024-11-22 12:21:18 +0200

    Scroll positions restoration enabled.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {InMemoryScrollingOptions} from "@angular/router";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
